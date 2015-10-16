43407258a5c8ec091c347bd95db591a8443025cb
Lydia Guarino <lydia@frontside.io>
2015-10-16 02:30:23 -0500

    navigation added

--- app/components/stats-bar.js
+++ app/components/stats-bar.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   convertedValue: Ember.computed('value', function(){
-    return this.get('value') * 20 - 2;
+    return Math.max(this.get('value') * 20 - 2, 0);
   })
 });
--- app/routes/scenarios/scenario.js
+++ app/routes/scenarios/scenario.js
@@ -3,5 +3,9 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.modelFor('scenarios').findBy('id', parseInt(params.id));
+  },
+  afterModel: function(model, transition) {
+    // TODO: handle redirecting to correct nested route when switching scenario routes
+    this.transitionTo('scenarios.scenario.stats', model);
   }
 });
