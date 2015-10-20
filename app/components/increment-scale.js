c781fcb75b8e13432f0018087b89b6143996ecbd
Lydia Guarino <lydia@frontside.io>
2015-10-20 11:41:56 -0500

    modify scale added

--- app/components/increment-scale.js
+++ app/components/increment-scale.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  convertedValue: Ember.computed('value', function(){
+    return Math.max(this.get('value') * 20 - 2, 0);
+  }),
+  actions: {
+    increment(){
+      let value = this.get('value');
+      if(value < 10){
+        this.set('value', value + 1);
+      }
+    },
+    decrement(){
+      let value = this.get('value');
+      if(0 < value){
+        this.set('value', value - 1);
+      }
+    }
+  }
 });
